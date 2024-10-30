f53d715e9bac40dc7404b1c31c098cdd23835819
Marcos Hernández <126048022+marcosnigp1@users.noreply.github.com>
2024-10-31 00:46:51 +0400

    Added a quick audio analyzer and a fixed menu

--- sketch.js
+++ sketch.js
@@ -1,21 +1,49 @@
 //Examples taken from:https://therewasaguy.github.io/p5-music-viz/ 
+//Sound example taken from the following video: https://www.youtube.com/watch?v=2O3nm0Nvbi4
+
+//Preparing variables for the sound analyzer.
+let song;
+let amperes;
+let button;
+
+let fft;
+
+function preload(){
+  audio = new loadSound("media/musicstest.mp3");
+}
+
 
 function setup() {
   createCanvas(400, 400);
 
   //Continously adjust website size to canvas.
   windowResized();
+  fft = new p5.FFT(0.5, 256);
+
+  //Play audio.
+  audio.loop();
 }
 
 function draw() {
   background(0);
 
-  //Continously adjust canvas to screen size.
+  var spectrum = fft.analyze();
+  stroke(255);
+  for (let i = 0; i<spectrum.length; i++){
+    let amperes = spectrum[i];
+    let y = map(amperes, 0,1, height, 0);
+    line(i, height, i, y);
+  }
+
+  //Testing text.
   push();
   fill(255);
-  text("This is a test", width*0.46, height*0.40);
+  text("This is a test", width*0.42, height*0.40);
   pop();
 }
+
+
+
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
